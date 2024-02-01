fc017cc4e954b80fcbabd4da861b701b5496fb50
Lucas Egidio <lucasegidio1@gmail.com>
2024-02-01 04:36:02 +0100

    New layout

--- dark_mode_manager.js
+++ dark_mode_manager.js
@@ -1,11 +1,10 @@
-$("#dark-mode-sw").on( "change", function() {
+  $("#dark-mode-sw").on("change", function () {
     if ($("#dark-mode-sw input")[0].checked)
       $("html").attr("data-bs-theme", "dark");
     else
       $("html").attr("data-bs-theme", "light");
-      
-  } );
 
+  });
 
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
